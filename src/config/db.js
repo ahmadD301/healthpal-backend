4394612a683b717ac942ae1e521dfd248e4bba48
yazan339 <yoyoyazan22@gmail.com>
2025-11-26 19:32:37 +0200

    testing database connection configuration

--- src/config/db.js
+++ src/config/db.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
@@ -22,5 +23,8 @@ export const connectDB = async () => {
     console.error("❌ Unable to connect to the database:", error);
   }
 };
+
+
+
 
 export default sequelize;
